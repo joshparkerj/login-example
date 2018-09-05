92b0c2263ff7e3da5714b180aeef0249e1a1175b
joshparkerj <superfakek@gmail.com>
2018-09-04 18:55:03 -0600

    create home and login views

---
+++ src/views/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+
+  render() {
+    return (
+      <div>Hello from Home.js!</div>
+    )
+  }
+}
+
+export default Home;
---
+++ src/views/Login.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+
+  render() {
+    return (
+      <div>Hello from Login.js!</div>
+    )
+  }
+}
+
+export default Login;
